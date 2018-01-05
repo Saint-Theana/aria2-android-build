b49d1f412adce05fef17191666b6e08b5de0f8bd
Saint-Theana <34052138+Saint-Theana@users.noreply.github.com>
2018-01-05 14:49:06 +0000

    Update and rename ndk-toolchain-build.sh to build-ndk-toolchain.sh

---
+++ aarch64-android/build-ndk-toolchain.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+mkdir /opt/NDK
+cd /opt/NDK
+apt install wget
+wget https://dl.google.com/android/repository/android-ndk-r15c-linux-x86_64.zip
+apt install unzip
+unzip android-ndk-r15c-linux-x86_64.zip
+rm android-ndk-r15c-linux-x86_64.zip
+
+echo "export NDK=/opt/NDK/android-ndk-r15c
+export ANDROID_HOME=/opt/NDK/android-ndk-r15c
+export PATH=$ANDROID_HOME:$PATH" >> /etc/profile
+source /etc/profile
+
+$NDK/build/tools/make_standalone_toolchain.py \
+   --arch arm64 --api 21 --stl=gnustl \
+   --install-dir $ANDROID_HOME/toolchain
+
+cd $ANDROID_HOME
+
+mkdir -p usr/local/lib/pkgconfig
